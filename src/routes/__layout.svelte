fb5ac5d9a801101e0fc9f4cdd83dcbd009f02d11
Angel Chavez <angeldchz@gmail.com>
2022-07-20 12:58:50 -0400

    modified grid system

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,19 +6,28 @@
   <layout class="layout-grid">
     <navbar
       class="shadow-level-1 align-items-center justify-content-between px-xl-c py-xs-c"
-      style="--row-gap: var(--bp-md-spacing-c); --column-gap: var(--bp-md-spacing-c);"
     >
       <div class="flex ft-parragraph-c"> Custom Route </div>
       <div class="flex col-xl-gap-for-xs row">
         <button
-          class="ft-parragraph-c flex col-xs-6  no-wrap border-0 bg-clr-secondary-brand-yellow-500 bg-clr-primary-brand-blue-500-active flex-grow-1"
+          class="ft-parragraph-c flex col-xs-6 col-sm-4  border-0 bg-clr-secondary-brand-yellow-500 bg-clr-primary-brand-blue-500-active flex-grow-1"
         >
-          second entry
+          hello there
         </button>
         <button
-          class="ft-parragraph-c flex  col-xs-6no-wrap  border-0 bg-clr-secondary-brand-yellow-500 flex-grow-1"
+          class="ft-parragraph-c flex col-xs-6 col-sm-4  border-0 bg-clr-secondary-brand-yellow-500 bg-clr-primary-brand-blue-500-active flex-grow-1"
         >
-          third entry
+          hello there
+        </button>
+        <button
+          class="ft-parragraph-c flex col-xs-6 col-sm-4  border-0 bg-clr-secondary-brand-yellow-500 bg-clr-primary-brand-blue-500-active flex-grow-1"
+        >
+          hello there
+        </button>
+        <button
+          class="ft-parragraph-c flex col-xs-6 col-sm-4  border-0 bg-clr-secondary-brand-yellow-500 bg-clr-primary-brand-blue-500-active flex-grow-1"
+        >
+          hello there
         </button>
       </div>
     </navbar>
