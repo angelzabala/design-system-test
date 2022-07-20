d4cc26c068e30ac53b1a293fb625cff5b34dffba
Angel Chavez <angeldchz@gmail.com>
2022-07-20 00:29:21 -0400

    added small documentation and adjusted grid

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,20 +3,20 @@
 </script>
 
 <div class="vw-100 vh-100 bg-clr-semantic-neutral-abswhite" id="application-layout">
-  <layout class="layout-grid d-grid">
+  <layout class="layout-grid">
     <navbar
-      class="shadow-level-1 d-grid align-items-center justify-content-between px-xl-c py-xs-c"
+      class="shadow-level-1 align-items-center justify-content-between px-xl-c py-xs-c"
       style="--row-gap: var(--bp-md-spacing-c); --column-gap: var(--bp-md-spacing-c);"
     >
       <div class="flex ft-parragraph-c"> Custom Route </div>
-      <div class="flex">
+      <div class="flex col-xl-gap-for-xs row">
         <button
-          class="ft-parragraph-c flex  no-wrap border-0 bg-clr-secondary-brand-yellow-500 flex-grow-1"
+          class="ft-parragraph-c flex col-xs-6  no-wrap border-0 bg-clr-secondary-brand-yellow-500 bg-clr-primary-brand-blue-500-active flex-grow-1"
         >
           second entry
         </button>
         <button
-          class="ft-parragraph-c flex no-wrap  border-0 bg-clr-secondary-brand-yellow-500 flex-grow-1"
+          class="ft-parragraph-c flex  col-xs-6no-wrap  border-0 bg-clr-secondary-brand-yellow-500 flex-grow-1"
         >
           third entry
         </button>
